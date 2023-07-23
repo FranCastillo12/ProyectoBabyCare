0a95d983191ae1ff5356a89990741457289dc376
Laskee <sebas0803@outlook.com>
2023-07-23 04:34:17 -0600

    Se ha agregado media funcionalidad de la pantalla expediente y se han ingresado nuevos procedimientos almacenados, tambien se incluye la indexacion de las pantallas publicas y la de la pantalla expedientes

---
+++ Entidades/Expediente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Expediente
+    {
+        private int idexpediente;
+        private string nombrebebe;
+        private int idbebe;
+        private string cedula;
+        private string nombrePadre;
+        private string nombreMadre;
+        private float peso;
+        private float estatura;
+        private string tiposangre;
+        private string genero;
+        private string fechanacimiento;
+
+        public int Idexpediente { get => idexpediente; set => idexpediente = value; }
+        public string Nombrebebe { get => nombrebebe; set => nombrebebe = value; }
+        public int Idbebe { get => idbebe; set => idbebe = value; }
+        public string Cedula { get => cedula; set => cedula = value; }
+        public string NombrePadre { get => nombrePadre; set => nombrePadre = value; }
+        public string NombreMadre { get => nombreMadre; set => nombreMadre = value; }
+        public float Peso { get => peso; set => peso = value; }
+        public float Estatura { get => estatura; set => estatura = value; }
+        public string Tiposangre { get => tiposangre; set => tiposangre = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Fechanacimiento { get => fechanacimiento; set => fechanacimiento = value; }
+    }
+}
---
+++ ProyectoBabyCare/pages/Expediente.aspx.cs
@@ -0,0 +1,40 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoBabyCare.pages
+{
+    public partial class Expediente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int idbebe=0;
+            string correo = "";
+            if (Session["Credenciales"]!=null && Session["idbebe"]!=null) {
+                Entidades.En_Usuarios usuario = (Entidades.En_Usuarios)Session["Credenciales"];
+                correo = usuario.Usuario;
+                idbebe = Convert.ToInt32(Convert.ToString(Session["idbebe"]));
+            }
+            if (!IsPostBack)
+            {
+                if (idbebe != 0 && !correo.Equals("")) { 
+                    Entidades.Expediente expediente= new Entidades.Expediente();
+                    Negocios.Expediente ex=new Negocios.Expediente();
+                    expediente = ex.obtenerexpediente(correo,idbebe);
+
+                    txtnombre.Text = expediente.Nombrebebe;
+                    txtPeso.Text=Convert.ToString(expediente.Peso);
+                    txtSangre.Text = expediente.Tiposangre;
+                    txtpapa.Text = expediente.NombrePadre;
+                    txtmama.Text = expediente.NombreMadre;
+                }
+
+            }
+        }
+    }
+}
