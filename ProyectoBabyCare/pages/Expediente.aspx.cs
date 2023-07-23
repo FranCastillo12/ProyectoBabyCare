84954ae93f61934b7a39de13eab57e25386823bb
Laskee <sebas0803@outlook.com>
2023-07-23 16:59:06 -0600

    He agregado los elementos correspondientes del expediente del bebe a la plantilla del expediente, faltan los eventos de los botones.

--- ProyectoBabyCare/pages/Expediente.aspx.cs
+++ ProyectoBabyCare/pages/Expediente.aspx.cs
@@ -15,26 +15,96 @@ namespace ProyectoBabyCare.pages
         {
             int idbebe=0;
             string correo = "";
-            if (Session["Credenciales"]!=null && Session["idbebe"]!=null) {
+            if (Session["Credenciales"]!=null) {
                 Entidades.En_Usuarios usuario = (Entidades.En_Usuarios)Session["Credenciales"];
                 correo = usuario.Usuario;
-                idbebe = Convert.ToInt32(Convert.ToString(Session["idbebe"]));
+                idbebe = Convert.ToInt32(Convert.ToString(usuario.IdenBebe));
             }
-            if (!IsPostBack)
+            Negocios.Expediente ex = new Negocios.Expediente();
+            List<Entidades.Padecimientos> lstpadecimientos = new List<Padecimientos>();
+            lstpadecimientos = ex.TodoslosPadecimientos();
+            dpllPadecimientos.Items.Add("Seleccione un padecimiento");
+            foreach (Entidades.Padecimientos ex1 in lstpadecimientos)
             {
-                if (idbebe != 0 && !correo.Equals("")) { 
-                    Entidades.Expediente expediente= new Entidades.Expediente();
-                    Negocios.Expediente ex=new Negocios.Expediente();
-                    expediente = ex.obtenerexpediente(correo,idbebe);
+                dpllPadecimientos.Items.Add(ex1.IdPadecimiento + "-" + ex1.Nombrepadecimiento);
+            }
+            if (idbebe != 0 && !correo.Equals(""))
+            {
+                Entidades.Expediente expediente = new Entidades.Expediente();
+                
+                expediente = ex.obtenerexpediente(correo, idbebe);
 
-                    txtnombre.Text = expediente.Nombrebebe;
-                    txtPeso.Text=Convert.ToString(expediente.Peso);
-                    txtSangre.Text = expediente.Tiposangre;
-                    txtpapa.Text = expediente.NombrePadre;
-                    txtmama.Text = expediente.NombreMadre;
+                txtnombre.Text = expediente.Nombrebebe;
+                txtPeso.Text = Convert.ToString(expediente.Peso);
+                txtSangre.Text = expediente.Tiposangre;
+                txtpapa.Text = expediente.NombrePadre;
+                txtmama.Text = expediente.NombreMadre;
+                txtestatura.Text = Convert.ToString(expediente.Estatura);
+                txtfecha.Text = expediente.Fechanacimiento;
+
+                //padecimientos
+                
+                List<Entidades.Padecimientos> lstpadecimientosexpediente = new List<Padecimientos>();              
+                lstpadecimientosexpediente = ex.PadecimientosExpediente(expediente.Idexpediente);
+                
+                txtPadecimientos.Text = "";
+                if (lstpadecimientosexpediente.Count > 0)
+                {
+                    int cont = 1;
+                    foreach (Entidades.Padecimientos ex2 in lstpadecimientosexpediente)
+                    {
+                        txtPadecimientos.Text +=cont+"-"+ ex2.Nombrepadecimiento + "\n";
+                        cont++;
+                    }
+                }
+                else {
+                    txtPadecimientos.Text = "Sin registros";
+                }
+                //detalles
+                txtDetalles.Text = "";
+                string[] lstdetalle = ex.Detalles(expediente.Idexpediente);
+                if (lstdetalle.Length > 0)
+                {
+                    int cont = 1;
+                    foreach (string s in lstdetalle)
+                    {
+                        if (!s.Equals("")) { 
+                            string[] atrib = s.Split('@');
+                            string[] fecha = atrib[1].Split(' ');
+                            txtDetalles.Text += $"{cont}-{fecha[0]}: {atrib[0]}\n";
+                            cont++;
+                        }
+                        
+                    }
+                }
+                else {
+                    txtDetalles.Text = "Sin registros";
+                }
+                //Vacunas
+                txtVacunas.Text = "";
+                string[] lstvacunas = ex.Vacunas(expediente.Idexpediente);
+                if (lstdetalle.Length > 0)
+                {
+                    int cont = 1;
+                    foreach (string s in lstdetalle)
+                    {
+                        if (!s.Equals(""))
+                        {
+                            string[] atrib = s.Split('@');
+                            string[] fecha = atrib[1].Split(' ');
+                            txtVacunas.Text += $"{cont}-{fecha[0]}: {atrib[0]}\n";
+                            cont++;
+                        }
+
+                    }
+                }
+                else
+                {
+                    txtVacunas.Text = "Sin registros";
                 }
 
             }
+
         }
     }
 }
