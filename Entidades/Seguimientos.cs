b5e6d9ed54cc3489aaa81a89bebb3ea6e2d6a3df
Laskee <sebas0803@outlook.com>
2023-07-24 02:33:38 -0600

    Finalizada la funcionalidad del seguimiento de actividades(Sebas)

---
+++ Entidades/Seguimientos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Seguimientos
+    {
+        private string categoria;
+        private string descripcion;
+        private DateTime fecha;
+
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+    }
+}
