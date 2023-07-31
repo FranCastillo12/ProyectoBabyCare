f263517fa2af62908568176fba2b220122309457
Francisco Castillo <castillof075@gmail.com>
2023-07-31 01:13:18 -0600

    cambios

--- ProyectoBabyCare/SitePrivate.Master.designer.cs
+++ ProyectoBabyCare/SitePrivate.Master.designer.cs
@@ -33,15 +33,6 @@ namespace ProyectoBabyCare
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Control expediente.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor expediente;
-
-        /// <summary>
         /// Control navbarDropdownMenuLink1.
         /// </summary>
         /// <remarks>
