6b80934336c3abedc48b577492bbc432f57d35ab
Laskee <sebas0803@outlook.com>
2023-08-13 18:40:46 -0600

    Configuraciones del sistema

---
+++ ProyectoBabyCare/pages/Configuraciones.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProyectoBabyCare.pages
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                Negocios.Configuraciones config = new Negocios.Configuraciones();
+                Entidades.ConfiguracionesSistema configsistema = new Entidades.ConfiguracionesSistema();
+                Entidades.ConfiguracionesGrupoFamiliar configgrupo = new Entidades.ConfiguracionesGrupoFamiliar();
+                configsistema = config.TraerConfiguraciones();
+                configgrupo = config.TraerGrupoFamiliar();
+                //Cargar datos del sistema
+                txtAlertas.Text = configsistema.Alertas.ToString();
+                txtFotos.Text = configsistema.Fotos.ToString();
+                txtVideos.Text = configsistema.Videos.ToString();
+                txtUltrasonidos.Text = configsistema.Ultrasonidos.ToString();
+
+                //Cargar datos del grupo familiar
+                txtPadres.Text = configgrupo.Padres.ToString();
+                txtMadres.Text = configgrupo.Madres.ToString();
+                txtAbuelos.Text = configgrupo.Abuelos.ToString();
+                txtBabysisters.Text = configgrupo.Babysisters.ToString();
+                txtTios.Text = configgrupo.Tios.ToString();
+                txtTias.Text = configgrupo.Tias.ToString();
+                txtInvitados.Text = configgrupo.Invitados.ToString();
+            }
+        }
+
+        protected void btnModificarGrupoFamiliar_Click(object sender, EventArgs e)
+        {
+            if (!txtPadres.Text.Trim().Equals("") && !txtMadres.Text.Trim().Equals("") && !txtTios.Text.Trim().Equals("") && !txtTias.Text.Trim().Equals("")
+                && !txtAbuelos.Text.Trim().Equals("") && !txtBabysisters.Text.Trim().Equals("") && !txtInvitados.Text.Trim().Equals("")) {
+
+                int padres = Convert.ToInt32(txtPadres.Text);
+                int madres = Convert.ToInt32(txtMadres.Text);
+                int abuelos = Convert.ToInt32(txtAbuelos.Text);
+                int babysisters = Convert.ToInt32(txtBabysisters.Text);
+                int tios = Convert.ToInt32(txtTios.Text);
+                int tias = Convert.ToInt32(txtTias.Text);
+                int invitadps = Convert.ToInt32(txtInvitados.Text);
+                Negocios.Configuraciones config = new Negocios.Configuraciones();
+                config.ModificarConfiguracionesGrupoFamiliar(padres, madres, abuelos, babysisters, tios, tias, invitadps);
+
+                Response.Redirect("Configuraciones.aspx");
+
+            }
+            else {
+               string scriptalerta =
+                        "toastr.options.closeButton = true;" +
+                         "toastr.options.positionClass = 'toast-bottom-right';" +
+                        "toastr.error('Ninguno de los campos del grupo familiar puede ir vacio');";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ToastrError", scriptalerta, true);
+            }
+        }
+
+        protected void btnModificarSistema_Click(object sender, EventArgs e)
+        {
+            if (!txtAlertas.Text.Trim().Equals("") && !txtFotos.Text.Trim().Equals("") && !txtVideos.Text.Trim().Equals("") && !txtUltrasonidos.Text.Trim().Equals(""))
+            {
+                int alertas= Convert.ToInt32(txtAlertas.Text);
+                int Fotos= Convert.ToInt32(txtFotos.Text);
+                int Videos= Convert.ToInt32(txtVideos.Text);
+                int Ultrasonidos= Convert.ToInt32(txtUltrasonidos.Text);
+                Negocios.Configuraciones config = new Negocios.Configuraciones();
+                config.ModificarConfiguracionesSistema(alertas,Fotos,Videos,Ultrasonidos);
+
+                Response.Redirect("Configuraciones.aspx");
+            }
+            else
+            {
+                string scriptalerta =
+                        "toastr.options.closeButton = true;" +
+                         "toastr.options.positionClass = 'toast-bottom-right';" +
+                        "toastr.error('Ninguno de los campos de configuraciones del sistema puede ir vacio');";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ToastrError", scriptalerta, true);
+            }
+            
+
+        }
+    }
+}
---
+++ ProyectoBabyCare/pages/Configuraciones.aspx.designer.cs
@@ -0,0 +1,233 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoBabyCare.pages
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control txtPadres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPadres;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control txtMadres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMadres;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control txtTios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTios;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control txtTias.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTias;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control txtAbuelos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAbuelos;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control txtBabysisters.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBabysisters;
+
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Control txtInvitados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtInvitados;
+
+        /// <summary>
+        /// Control btnModificarGrupoFamiliar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificarGrupoFamiliar;
+
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+
+        /// <summary>
+        /// Control txtUltrasonidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUltrasonidos;
+
+        /// <summary>
+        /// Control Label9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+
+        /// <summary>
+        /// Control txtAlertas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAlertas;
+
+        /// <summary>
+        /// Control Label10.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+
+        /// <summary>
+        /// Control txtFotos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFotos;
+
+        /// <summary>
+        /// Control Label11.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
+        /// Control txtVideos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtVideos;
+
+        /// <summary>
+        /// Control btnModificarSistema.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificarSistema;
+    }
+}
