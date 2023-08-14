4413b64fc916eb47695005d8daf9dae267ac8ebb
Francisco Castillo <castillof075@gmail.com>
2023-08-13 23:47:27 -0600

    alertas en las pantallas

--- ProyectoBabyCare/pages/Perfil.aspx.cs
+++ ProyectoBabyCare/pages/Perfil.aspx.cs
@@ -11,6 +11,7 @@ namespace ProyectoBabyCare.pages
 {
     public partial class Perfil : System.Web.UI.Page
     {
+        int idBebe = 0;
         string nombrebebe;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -61,6 +62,27 @@ namespace ProyectoBabyCare.pages
                 }
                 SitePrivate masterPage = (SitePrivate)this.Master;
                 masterPage.MiMetodoEnMasterPage();
+
+
+                //Llama al metodo para activar las alertas y mostrar mensaje
+                Negocios.AlertasUsuario alert = new Negocios.AlertasUsuario();
+                DateTime horaActual = DateTime.Now;
+                alert.ActivateAlertas(horaActual, idBebe);
+                List<Entidades.Alerta> alertas = alert.TraerAlertas(idBebe);
+
+                string scriptalerta = null;
+                foreach (Entidades.Alerta alrt in alertas)
+                {
+                    if (alrt.HoraDeAlerta.TimeOfDay <= horaActual.TimeOfDay && alrt.Estado == true)
+                    {
+                        scriptalerta =
+                    "toastr.options.closeButton = true;" +
+                     "toastr.options.positionClass = 'toast-bottom-right';" +
+                    $"toastr.warning('Hay una alerta pendiente en estos momentos! ({alrt.HoraDeAlerta.ToString("hh:mm tt")})');";
+                        ScriptManager.RegisterStartupScript(this, GetType(), "ToastrWarning", scriptalerta, true);
+                    }
+                }
+                // Final del metodo de mostrar alertas
             }
         }
 
--- ProyectoBabyCare/pages/Ultrasonidos.aspx.cs
+++ ProyectoBabyCare/pages/Ultrasonidos.aspx.cs
@@ -63,6 +63,27 @@ namespace ProyectoBabyCare.pages
 
                 //Agrega el código HTML a la página web para mostrar las cartas
                 this.lstfrmMantenimiento.InnerHtml = strListaProductos.ToString();
+
+                //Llama al metodo para activar las alertas y mostrar mensaje
+                Negocios.AlertasUsuario alert = new Negocios.AlertasUsuario();
+                DateTime horaActual = DateTime.Now;
+                alert.ActivateAlertas(horaActual, idBebe);
+                List<Entidades.Alerta> alertas = alert.TraerAlertas(idBebe);
+
+                string scriptalerta = null;
+                foreach (Entidades.Alerta alrt in alertas)
+                {
+                    if (alrt.HoraDeAlerta.TimeOfDay <= horaActual.TimeOfDay && alrt.Estado == true)
+                    {
+                        scriptalerta =
+                    "toastr.options.closeButton = true;" +
+                     "toastr.options.positionClass = 'toast-bottom-right';" +
+                    $"toastr.warning('Hay una alerta pendiente en estos momentos! ({alrt.HoraDeAlerta.ToString("hh:mm tt")})');";
+                        ScriptManager.RegisterStartupScript(this, GetType(), "ToastrWarning", scriptalerta, true);
+                    }
+                }
+                // Final del metodo de mostrar alertas
+
             }
         }
 
